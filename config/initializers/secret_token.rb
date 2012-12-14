471e567e258548cdfabd5af1e3a3d133a5dda9de
Jason Rylance <ecnalyr@gmail.com>
2012-12-14 09:20:34 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhatsNewApp::Application.config.secret_token = 'fc43cc803c4e8bf9f2e0f620f3054807b2b3e9614f06c3f3dc7cb5c32b85e131cc5f885388277827dda3ec32836f769af7f0fe52256803e44266c3c0c36ebe85'
