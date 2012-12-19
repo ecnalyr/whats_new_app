6aeb9f376de38d157954b3bd90bc104b2f5add22
Jason Rylance <ecnalyr@gmail.com>
2012-12-19 13:00:47 -0600

    Added a "Store" value to Products

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121214153701) do
+ActiveRecord::Schema.define(:version => 20121219184427) do
 
   create_table "products", :force => true do |t|
     t.string   "name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(:version => 20121214153701) do
     t.string   "sku"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.string   "store"
   end
 
 end
