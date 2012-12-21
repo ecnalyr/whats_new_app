b6ab38d8fb0e26b3a3356aba2d7cfd86f7e85d14
Jason Rylance <ecnalyr@gmail.com>
2012-12-20 19:31:54 -0600

    Made :store accessible by Products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,3 @@
 class Product < ActiveRecord::Base
-  attr_accessible :imageLink, :link, :name, :price, :scrape_time, :sku
+  attr_accessible :imageLink, :link, :name, :price, :scrape_time, :sku, :store
 end
