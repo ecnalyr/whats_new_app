d0c4cab5a11347d935a19bb70dade800f737c7a5
Jason Rylance <ecnalyr@gmail.com>
2012-12-22 02:06:26 -0600

    Added a size to restrict images and added a condensed view of products at the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
   def index
+    @products = Product.order("scrape_time desc")
+
+    respond_to do |format|
+      format.html {}# index.html.erb
+      format.json { render json: @products }
+    end
   end
 end
