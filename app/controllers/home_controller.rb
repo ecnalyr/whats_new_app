1e44f98164e15832350360e444e68643a7ec2b26
Jason Rylance <ecnalyr@gmail.com>
2012-12-23 14:48:00 -0600

    Each date of updated products has its own table.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
-    @products = Product.order("scrape_time desc")
+    # @products = Product.order("scrape_time desc")
+    @products = Product.all.group_by { |product| product.updated_at.to_date}
 
     respond_to do |format|
       format.html {}# index.html.erb
