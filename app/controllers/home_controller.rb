e7741283f6bec5c3969f6be6690eb02d611187b1
Jason Rylance <ecnalyr@gmail.com>
2013-01-07 12:32:47 -0600

    Added a limit of 150 items to the product query

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -14,7 +14,10 @@ class HomeController < ApplicationController
       session[:stores] = @selected_stores
       redirect_to :stores => @selected_stores and return
     end
-    @products = Product.order("created_at desc").find_all_by_store(@selected_stores.keys).group_by { |product| product.created_at.to_date}
+    # There is a limit of 150 items here for development reasons
+    #   - Loading every single item in the database plus their pictures is stressful.
+    #   - this needs to be moved to a variable somewhere.
+    @products = Product.order("created_at desc").limit(150).find_all_by_store(@selected_stores.keys).group_by { |product| product.created_at.to_date}
     @last_visit = Time.parse(cookies[:last_visit]).utc - last_visit_buffer.minutes
     now = Time.now.utc
 
