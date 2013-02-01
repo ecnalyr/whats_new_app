10ac5c04459757bfa1402782d677959715ec36de
Jason Rylance <ecnalyr@gmail.com>
2013-02-01 09:26:44 -0600

    Added a before_filter to build initial cookie

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :first_visit
+
+  def first_visit
+    if cookies[:last_visit] == ""
+      update_last_visit
+    end
+  end
 
   def update_last_visit
     cookies.permanent[:last_visit] = Time.now.utc
