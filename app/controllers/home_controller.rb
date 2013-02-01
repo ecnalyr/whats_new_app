908d44028bd29bf22242d8c57e7845e4944d57be
Jason Rylance <ecnalyr@gmail.com>
2013-02-01 09:04:24 -0600

    Trying to resolve null products issue.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,10 @@ class HomeController < ApplicationController
 
     @products = Product.order("created_at desc").group_by { |product| product.store}
 
+    if @products.count == 0
+      @products = Product.new(:name => 'name', :store => 'store', :price => 1.15)
+    end
+
     @last_visit = Time.parse(cookies[:last_visit]).utc - last_visit_buffer.minutes
     now = Time.now.utc
 
