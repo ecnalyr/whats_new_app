820530b93e27db2a099b3dedc809bfcf48064e7f
Jason Rylance <ecnalyr@gmail.com>
2013-02-02 14:16:07 -0600

    Tweaked 'new item' badge positioning

--- app/helpers/latest_products_helper.rb
+++ app/helpers/latest_products_helper.rb
@@ -3,7 +3,7 @@ module LatestProductsHelper
   def new_item_badge(created_at, last_visit)
     out = ''
     if created_at >= last_visit
-      out << '<span class="label label-warning">new item</span>'
+      out << '<span class="label label-warning pull-right">new item</span>'
     else
       out.html_safe
     end
