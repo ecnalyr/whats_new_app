d845fa6b07212c7cbe904b8c360ff6e05eb9b0b0
Jason Rylance <ecnalyr@gmail.com>
2013-02-02 20:58:45 -0600

    Added AE Women's as a product's store.

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ActiveRecord::Base
   attr_accessible :imageLink, :link, :name, :price, :scrape_time, :sku, :store
 
   def self.all_stores
-    %w(Sephora Forever\ 21)
+    %w(Sephora Forever\ 21 AE\ Women's)
   end
 end
 
