457a49753e7dab1bca83c7962c86484a5c0e39f2
Jason Rylance <ecnalyr@gmail.com>
2013-02-21 09:57:53 -0600

    Added the option to switch between grid and list views within latest_products

--- app/controllers/latest_products_controller.rb
+++ app/controllers/latest_products_controller.rb
@@ -15,13 +15,18 @@ class LatestProductsController < ApplicationController
       session[:stores] = params[:stores]
       redirect_to :stores => @selected_stores and return
     end
-    # There is a limit of 150 items here for development reasons
-    #   - Loading every single item in the database plus their pictures is stressful.
-    #   - this needs to be moved to a variable somewhere.
+    
     @products = Product.order("created_at desc").limit(@number_of_products).find_all_by_store(@selected_stores.keys).group_by { |product| product.created_at.to_date}
     @last_visit = Time.parse(cookies[:last_visit]).utc - last_visit_buffer.minutes
     now = Time.now.utc
 
+    if !params[:view].nil?
+      @partial = params[:view]
+      session[:view] = params[:view]
+    else
+      @partial = session[:view] || "grid"
+    end
+    
     logger.info(params)
     logger.info("^^^ THERE WERE YOUR PARAMS AHHHH!!!!!")
 
