530d816e3f10ef08bdfe4b32cd60440fbdc7273b
Jason Rylance <ecnalyr@gmail.com>
2013-02-21 18:37:42 -0600

    Added background highlighting to home page products that are new

--- app/helpers/latest_products_helper.rb
+++ app/helpers/latest_products_helper.rb
@@ -10,4 +10,13 @@ module LatestProductsHelper
     out.html_safe
   end
 
+  def new_item?(created_at, last_visit)
+    out = ''
+    if created_at >= last_visit
+      out <<"'product_summary row-fluid new'"
+    else
+      out <<"'product_summary row-fluid'"
+    end
+    out.html_safe
+  end
 end
